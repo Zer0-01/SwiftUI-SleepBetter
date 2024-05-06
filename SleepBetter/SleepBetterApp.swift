689519cdcd73657d94b9571decda397bf2f79853
Anas Zulkifli Mohd Jeffry <anas.zulkifli2001@gmail.com>
2024-05-06 14:43:49 +0800

    first commit

---
+++ SleepBetter/SleepBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepBetterApp.swift
+//  SleepBetter
+//
+//  Created by Silentmode Sdn Bhd on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SleepBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
